6eaf53464c1f7dcabf8b45dfe40ec836617cc37d
Sergei <mizuno338@mail.ru>
2023-12-27 12:24:08 +0300

    versionWithInputPhoneInput

--- src/sushiRollsFinalRedux/Cart/PhoneNumberInput/PhoneNumberInput.jsx
+++ src/sushiRollsFinalRedux/Cart/PhoneNumberInput/PhoneNumberInput.jsx
@@ -1,23 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const PhoneNumberInput = () => {
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [phone, setPhone] = useState("");
   const handleChange = (e) => {
-    const { value } = e.target;
-    console.log(value)
-    const phoneNumberPattern = /^(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){5,12}\d$/;
+    let value = e.target.value;
 
-    if (value === '' || phoneNumberPattern.test(value)) {
-      setPhoneNumber(value);
+    // Удаляем все символы, кроме цифр
+    value = value.replace(/\D/g, "");
+
+    // Проверяем, начинается ли номер с +7 или 8
+    if (
+      value.startsWith("7") ||
+      value.startsWith("8") ||
+      value.startsWith("+")
+    ) {
+      // Если номер начинается с 8, заменяем его на +7
+      if (value.startsWith("8", "1", "2", "3", "4", "5", "6", "7", "9", "0")) {
+        value = "8" + value.slice(1);
+      }
+    } else {
+      value = "";
     }
-  };
 
+    // Добавляем тире между цифрами
+    if (value.length >= 1) {
+      value = value.slice(0, 1) + "-" + value.slice(1);
+    }
+    if (value.length >= 6) {
+      value = value.slice(0, 5) + "-" + value.slice(5);
+    }
+    if (value.length >= 10) {
+      value = value.slice(0, 9) + "-" + value.slice(9);
+    }
+    if (value.length >= 13) {
+      value = value.slice(0, 12) + "-" + value.slice(12);
+    }
+    console.log(value.length);
+
+    setPhone(value);
+  };
   return (
     <input
-      type="text"
-      placeholder="+7(000)000-00-00"
-      value={phoneNumber}
+      type="tel"
+      value={phone}
       onChange={handleChange}
+      maxLength={15}
+      placeholder="+7(000)000-00-00"
     />
   );
 };
