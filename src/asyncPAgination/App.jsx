53dc43509939f75cbce7a43bc461b6d342cac6aa
Sergei <mizuno338@mail.ru>
2024-01-15 17:31:15 +0300

    AsyncPagination

--- src/asyncPAgination/App.jsx
+++ src/asyncPAgination/App.jsx
@@ -8,19 +8,36 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [coutriesPerPage] = useState(10);
-
+  const [error, setError] = useState(null);
   useEffect(() => {
     const getCountries = async () => {
-      setLoading(true);
-      const resp = await axios.get("https://restcountries.com/v3.1/all");
-      setCountries(resp.data);
-      setLoading(false);
+      try {
+        setLoading(true);
+        const resp = await axios
+          .get("https://restcountries.com/v3.1/all")
+          .catch((error) => console.log(error));
+        setCountries(resp.data);
+      } catch (error) {
+        setError("Ошибка получения стран!");
+      } finally {
+        setLoading(false);
+      }
     };
     getCountries();
   }, []);
 
-  const lastCountryIndex = coutriesPerPage * currentPage;
-  const firstCountryIndex = lastCountryIndex - coutriesPerPage;
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  const lastCountryIndex = Math.min(
+    coutriesPerPage * currentPage,
+    countries.length
+  );
+  const firstCountryIndex = Math.min(
+    lastCountryIndex - coutriesPerPage,
+    countries.length
+  );
   const currentCountry = countries.slice(firstCountryIndex, lastCountryIndex);
 
   const paginate = (pageNaumber) => setCurrentPage(pageNaumber);
@@ -29,19 +46,23 @@ const App = () => {
 
   return (
     <div className="container mt-5">
-      <h1 className="text-primary">Countries</h1>
+      <h1 className="text-primary">Страны</h1>
       <Countries countries={currentCountry} loading={loading} />
       <Pagination
         coutriesPerPage={coutriesPerPage}
         totalCountries={countries.length}
         paginate={paginate}
       />
-      <button className="btn-primary btn" onClick={prevPage}>
-        Предыдущая страница
-      </button>
-      <button className="btn-primary btn ms-2" onClick={nextPage}>
-        Следующая страница
-      </button>
+      {currentPage > 1 ? (
+        <button className="btn-primary btn" onClick={prevPage}>
+          Предыдущая страница
+        </button>
+      ) : null}
+      {currentPage < Math.ceil(countries.length / coutriesPerPage) ? (
+        <button className="btn-primary btn ms-2" onClick={nextPage}>
+          Следующая страница
+        </button>
+      ) : null}
     </div>
   );
 };
