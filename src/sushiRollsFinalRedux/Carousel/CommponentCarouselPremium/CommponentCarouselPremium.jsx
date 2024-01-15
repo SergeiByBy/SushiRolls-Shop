7b033a49e39b8344dc8bc0be7e96918f9a04d40e
Sergei <mizuno338@mail.ru>
2024-01-15 17:36:30 +0300

    new

--- src/sushiRollsFinalRedux/Carousel/CommponentCarouselPremium/CommponentCarouselPremium.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import premiumRollsCarouselImg from "../ImgCarousel/premiumRollsCarousel.webp"
-const PremiumRollsCarouselImg1 = () => {
-  return <div>
-  <img className="ImgCarousel" src={premiumRollsCarouselImg} alt="premiumRollsCarouselImg" />
-  </div>;
-};
-export default PremiumRollsCarouselImg1;
--- src/sushiRollsFinalRedux/Carousel/RollsCarousel/RollsCarousel.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import RollsCarousel1 from "../ImgCarousel/RollsCarousel.webp";
-const RollsCarousel = () => {
-  return <div>
-  <img className="ImgCarousel" src={RollsCarousel1} alt="RollsCarousel1" />
-  </div>;
-};
-export default RollsCarousel;
